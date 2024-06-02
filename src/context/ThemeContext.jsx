605134db82484f250f9108ba29596c75187494c9
nagendra kumar <rocky.naggi@gmail.com>
2024-06-02 12:11:25 +0530

    first commit

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -4,9 +4,8 @@ import { createContext ,useEffect,useState } from "react";
 export const ThemeContext = createContext();
 const getFromLocalStorage = () => {
     if (typeof window !== undefined) {
-        const value = localStorage.getItem("theme");
 
-        return value || "light";
+        return "light";
     }
 };
 
